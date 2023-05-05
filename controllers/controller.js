6261cc7b32ee7ff6c105b02373c2b81e065bc2e6
cmms-anabu-dev <123294219+cmms-anabu-dev@users.noreply.github.com>
2023-05-05 11:21:08 +0800

    updated load session attendance again

--- controllers/controller.js
+++ controllers/controller.js
@@ -293,7 +293,6 @@ const controller = {
             res.redirect("/login");
         }
         else{    
-            console.log(req.query.date);
             var date = new Date(req.query.date);
             var dateString;
             if (date.getMonth() < 9 && date.getDate() < 10){
@@ -316,10 +315,7 @@ const controller = {
             var nav = "<p id=\"navigation\"><a href=\"/sessions\">All Sessions</a> / " + spanDate + " - " + spanSession + "</p>"
 			console.log("Load Session Attendance - A");
             if (req.query.baptism == null){
-                console.log(date);
-                console.log(req.query.session);
-
-                db.findMany(Attendance, {date: date, session: req.query.session}, {}, (data) => {
+                db.findMany(Attendance, {date: req.query.date, session: req.query.session}, {}, (data) => {
                     var tempArray = [];
                     console.log(data);
                     console.log("-----");
